9c7e3524796bef43c10030f7323c90874b74095b
Cédric Schaller <cedric.schaller@elca.ch>
2021-11-23 14:19:18 +0100

    bug reproduction

---
+++ src/test/java/com/example/sleuthkafkatransactionmanager/SleuthKafkaTransactionManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sleuthkafkatransactionmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SleuthKafkaTransactionManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
